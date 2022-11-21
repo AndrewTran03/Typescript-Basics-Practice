0b4a87f10998810a808519d351d59ca56a00ba29
AndrewTran03 <andrewtranva@gmail.com>
2022-11-20 23:33:35 -0500

    Added Notes about How to Create Enums with TS

---
+++ enums.js
@@ -0,0 +1,12 @@
+"use strict";
+//Enums in TS
+exports.__esModule = true;
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice[SeatChoice["MIDDLE"] = 3] = "MIDDLE";
+    SeatChoice[SeatChoice["WINDOW"] = 4] = "WINDOW";
+    SeatChoice[SeatChoice["FOURTH"] = 5] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+;
+var mySeat = SeatChoice.AISLE;
---
+++ enums.ts
@@ -0,0 +1,13 @@
+//Enums in TS
+
+enum SeatChoice
+{
+    AISLE = "aisle",
+    MIDDLE = 3,
+    WINDOW,
+    FOURTH
+};
+
+const mySeat = SeatChoice.AISLE;
+
+export {}
